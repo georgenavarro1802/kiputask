e2d79fc9627a2b33d9627fffb8b4691e5f58091e
George Navarro <gnavarrog1802@gmail.com>
2016-11-01 23:05:26 -0400

    New html file that its the representation for custom error messages in e.g when 404 error occurs.  Changes in some files to include a comments to functions, decorators, routes, javascript, etc.

--- kipu.py
+++ kipu.py
@@ -1,22 +1,37 @@
+# Flask framework - install from requirements.txt
 from flask import Flask, render_template, jsonify
+
+# file that contains a json object representing the source code
 from vars import obj
 
+# determine root path
 app = Flask(__name__)
 
 
+# router that defines path: /home and returns an html file with the main user interface
 @app.route('/home')
 def home():
     return render_template("home.html")
 
 
+# router that defines path: /code and returns a json object with the source code of the app
 @app.route('/code')
 def code():
     return jsonify(**obj)
 
 
+# router that defines path: /getcode used by home.html via ajax function to return an html/text file with
+# the source code of the app and then transform it with prettify library and present data in python format to user
 @app.route('/getcode')
 def getcode():
     return render_template("source.html")
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
+# run the app where we only start the web server whenever this file called directly
 if __name__ == '__main__':
     app.run()
--- vars.py
+++ vars.py
@@ -1,31 +1,48 @@
 obj = {
     "code": {
         "file": "kipu.py",
-        "imports": {
-            "first_import": "from flask import Flask, render_template",
-            "second_import": "from vars import obj"
+        "imports": [
+            {
+                "descripcion_import": "from flask import Flask, render_template",
+                "comment_import": "# Flask framework - install from requirements.txt"
+            },
+            {
+                "descripcion_import": "from vars import obj",
+                "comment_import": "# file that contains a json object representing the source code"
+            },
+
+        ],
+
+        "app_definition": {
+            "definition": "app = Flask(__name__)",
+            "comment_definition": "# determine root path"
         },
-        "app_definition": "app = Flask(__name__)",
         "controllers": [
             {
                 "decorator": "@app.route('/home')",
                 "name": "def home():",
-                "content": "return render_template('home.html')"
+                "content": "return render_template('home.html')",
+                "comment": "# router that defines path: /home and returns an html file with the main user interface"
             },
             {
                 "decorator": "@app.route('/code')",
                 "name": "def code():",
-                "content": "return jsonify(**obj)"
+                "content": "return jsonify(**obj)",
+                "comment": "# router that defines path: /code and returns a json object with the source code of the app"
             },
             {
                 "decorator": "@app.route('/getcode')",
                 "name": "def getcode():",
-                "content": "return render_template('source.html')"
+                "content": "return render_template('source.html')",
+                "comment": "# router that defines path: /getcode used by home.html via ajax function "
+                           "to return an html/text file with the source code of the app and then transform "
+                           "it with prettify library and present data in python format to user"
             }
         ],
         "app_run": {
             "condition": "if __name__ == '__main__':",
-            "content": "app.run()"
+            "content": "app.run()",
+            "comment": "# run the app where we only start the web server whenever this file called directly"
         }
     }
 
